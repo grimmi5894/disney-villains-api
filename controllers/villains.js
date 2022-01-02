79cf2dcc6c88061af4178aba5482138b88905bec
Tricia Brooker <grimmi5894@gmail.com>
2022-01-02 08:26:47 -0500

    created villain.sql to set up database

--- controllers/villains.js
+++ controllers/villains.js
@@ -1,3 +1,9 @@
-const getAllVillains = () => {}
+const models = require('../models')
+
+const getAllVillains = async (request, response) => {
+  const villains = await models.villains.findAll()
+
+  return response.send(villains)
+}
 
 module.exports = { getAllVillains }
